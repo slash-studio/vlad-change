4c191b67f70021881b76742ae91a7f0d02bcc3de
Apanasenko <alexander.apanasenko@gmail.com>
2014-10-18 02:46:38 +1100

    Add placemark in map on dblclick

--- src/VladChange/MainBundle/Resources/public/js/map.js
+++ src/VladChange/MainBundle/Resources/public/js/map.js
@@ -1,9 +1,28 @@
 ymaps.ready(init);
 
-function init(){     
+function init(){    
+
     map = new ymaps.Map("map", {
         center: [43.15, 131.85],
         zoom: 11,
         controls: ['zoomControl']
+    });
+
+    map.events.add('dblclick', function (e) {
+        e.preventDefault();
+        var coords = e.get('coords');
+        map.geoObjects.add(
+            new ymaps.Placemark(
+                e.get('coords'), 
+                {
+                    balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+                }, 
+                {
+                    preset: 'islands#icon',
+                    iconColor: '#0095b6'
+                }
+            )
+        );
+        
     });
 }
