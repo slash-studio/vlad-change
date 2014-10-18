4bc8601752f1603a704ab861509781313f12ff41
Mark Tertishniy <mtertishniy@gmail.com>
2014-10-18 12:03:57 +1100

    Some adds with templates

--- src/VladChange/MainBundle/Controller/DefaultController.php
+++ src/VladChange/MainBundle/Controller/DefaultController.php
@@ -13,13 +13,20 @@ class DefaultController extends Controller
 
     public function viewProfileAction($id)
     {
-        $userName = $this->get('doctrine')->getManager()
-                         ->getRepository('VladChangeStoreBundle:User')
-                         ->findOneBy(['id' => $id])
-                         ->getUsername();
+        $authorized_user = $this->getUser();
+        if (!empty($authorized_user) && $authorized_user->getId() == $id) {
+            return $this->redirect($this->generateUrl('fos_user_profile_show'));
+        }
+        $user = $this->get('doctrine')
+                     ->getManager()
+                     ->getRepository('VladChangeStoreBundle:User')
+                     ->findOneBy(['id' => $id]);
+        if (empty($user)) {
+            return $this->redirect($this->generateUrl('vlad_change_main_homepage'));
+        }
         return $this->render(
             'VladChangeMainBundle:Default:profile.html.twig',
-            ['user_name' => $userName]
+            ['name' => $user->getName(), 'surname' => $user->getSurname()]
         );
     }
 }
