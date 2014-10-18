45f18fe4a329fee8043bc523c0096b2bd2730f95
Kalita Ivan <kaduev13@gmail.com>
2014-10-19 00:21:05 +1100

    Start working on edit project

--- src/VladChange/MainBundle/Controller/DefaultController.php
+++ src/VladChange/MainBundle/Controller/DefaultController.php
@@ -16,42 +16,47 @@ class DefaultController extends Controller
     public function editProjectAction(Request $request, $id)
     {
         $user = $this->getUser();
-        if (empty($user)) {
-            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        $placemark = $this->getDoctrine()
+                          ->getManager()
+                          ->getRepository('VladChangeStoreBundle:Placemark')
+                          ->findOneById($id);
+        if ($placemark == null || $placemark->getUser()->getId() != $user->getId()) {
+            return $this->redirect($this->generateUrl('vlad_change_main_homepage'));
         }
-        $form = $this->createForm(new PlacemarkType());
+        $form = $this->createForm(new PlacemarkType(), $placemark);
         $form->handleRequest($request);
-        return $this->render('VladChangeMainBundle:Pages:add_project.html.twig', [
-            'form' => $form->createView()
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->merge($form->getData());
+            $em->flush();
+            return $this->redirect($this->generateUrl('fos_user_profile_show'));
+        }
+        return $this->render('VladChangeMainBundle:Pages:edit_project.html.twig', [
+            'form' => $form->createView(),
+            'archived' => $placemark->getArchived(),
+            'expired'  => $placemark->isExpired(),
+            'notliked' => !$placemark->isLiked()
         ]);
     }
 
     public function addProjectAction(Request $request)
     {
         $user = $this->getUser();
-        if (empty($user)) {
-            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        $form = $this->createForm(new PlacemarkType());
+        $lat = $request->query->get('lat');
+        $lon = $request->query->get('lon');
+        if (!empty($lat) && !empty($lon)) {
+            $form->get('lat')->setData($lat);
+            $form->get('lon')->setData($lon);
         }
-        $view = null;
-        $isAvailableProj = $user->hasAvailableProjAmount();
-        if ($isAvailableProj) {
-            $form = $this->createForm(new PlacemarkType());
-            $lat = $request->query->get('lat');
-            $lon = $request->query->get('lon');
-            if (!empty($lat) && !empty($lon)) {
-                $form->get('lat')->setData($lat);
-                $form->get('lon')->setData($lon);
-            }
-            $form->handleRequest($request);
-            if ($form->isValid()) {
-                $em = $this->getDoctrine()->getManager();
-                $tmp = $form->getData();
-                $tmp->setUser($user);
-                $em->persist($tmp);
-                $em->flush();
-                return $this->redirect($this->generateUrl('fos_user_profile_show'));
-            }
-            $view = $form->createView();
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $tmp = $form->getData();
+            $tmp->setUser($user);
+            $em->persist($tmp);
+            $em->flush();
+            return $this->redirect($this->generateUrl('fos_user_profile_show'));
         }
         return $this->render('VladChangeMainBundle:Pages:add_project.html.twig', [
             'form' => $view,
