1aec6b8d0d85d051a6ebbbc4075e7b740dfc6b1d
smite107 <abramin@mail.ru>
2014-10-19 12:22:41 +1100

    Add profile css

--- src/VladChange/MainBundle/Resources/public/js/profile.js
+++ src/VladChange/MainBundle/Resources/public/js/profile.js
@@ -1,28 +1,12 @@
-$(function() {
-   function manipulateProject(t, _id) {
-      $.post(
-         '/project/' + t,
-         {
-            id: _id
-         },
-         function(data) {
-            //remove project from dom if
-         },
-         "json"
-      );
-   }
-
-   $(document).on('click', '.to_archive', function() {
-      manipulateProject('archive', $(this).attr('data-id'));
-      return false;
-   });
-
-   $(document).on('click', '.delete', function() {
-      manipulateProject('delete', $(this).attr('data-id'));
-      return false;
-   });
-
-   $(document).on('click', '.edit', function() {
-      $(location).attr('href', '/edit_project/' + $(this).attr('data-id'));
-   });
-});
+function manipulateProject(t, _id) {
+   $.post(
+      '/project/' + t,
+      {
+         id: _id
+      },
+      function(data) {
+         //remove project from dom if
+      },
+      "json"
+   );
+}
