a686ebba07636087223911ed2663c807646c59ac
Apanasenko <alexander.apanasenko@gmail.com>
2014-10-19 12:04:32 +1100

    Add map in profile page

--- src/VladChange/MainBundle/Resources/public/js/profile.js
+++ src/VladChange/MainBundle/Resources/public/js/profile.js
@@ -26,3 +26,42 @@ $(function() {
       $(location).attr('href', '/edit_project/' + $(this).attr('data-id'));
    });
 });
+
+ymaps.ready(init);
+
+function createPlacemark(info) {
+
+    var coords = [info.lat, info.lon];
+    placemark = new ymaps.Placemark(
+        coords,
+        {
+            name: info.name,
+            short_desc: info.shortDesc,
+        },
+        {
+            preset: 'islands#icon',
+            iconColor: '#0095b6',
+        }
+    );
+    return placemark;
+}
+
+function init() {
+
+    map = new ymaps.Map("map", {
+        center: [43.15, 131.85],
+        zoom: 11,
+        controls: ['zoomControl']
+    });
+
+    $.ajax({
+        url : "api/getAllPlacemark",
+        success: function(placemarks) {
+            for (i = 0; i < placemarks.length; i++) {
+                map.geoObjects.add(
+                    createPlacemark(placemarks[i])
+                );
+            }
+        }
+    })
+}
